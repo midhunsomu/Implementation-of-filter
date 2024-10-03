e2c3acd9aecec8e4f775abdd006fa094622d6c08
Midhun S <139331462+midhunsomu@users.noreply.github.com>
2024-10-03 15:48:16 +0530

    Update SHARPENING FILTERS.py

--- SHARPENING FILTERS.py
+++ SHARPENING FILTERS.py
@@ -2,33 +2,24 @@
 # In[4]: Using Laplacian Kernal
 
 
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-image1=cv2.imread("red.jpg")
-image2=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)
-kernel=np.ones((11,11),np.float32)/121
-image3=cv2.filter2D(image2,-1,kernel)
-kernel2=np.array([[-1,-1,-1],])
-image3=cv2.filter2D(image2,)
-plt.figure(figsize=(,8))
-plt.subplot()
-plt.imshow(image2)
-plt.title("Original Image")
+sharpened_image = cv2.filter2D(smoothed_image, -1, kernel2)
+plt.figure(figsize=(10, 8))
+plt.subplot(1, 2, 2)
+plt.imshow(sharpened_image)
+plt.title("Sharpened Image (Laplacian Kernel)")
 plt.axis("off")
-plt.subplot(1,2,)
-
+plt.show()
 
 # In[5]:Using Laplacian Operator
 
 
-laplacian=cv2.Laplacian(image2,)
-plt.figure(figsize=(8,8))
-plt.subplot()
-plt.imshow(image2)
-plt.title("Original Image")
+laplacian = cv2.Laplacian(image2, cv2.CV_64F)
+plt.figure(figsize=(10, 8))
+plt.subplot(1, 2, 2)
+plt.imshow(laplacian, cmap='gray')
+plt.title("Laplacian Operator Image")
 plt.axis("off")
-
+plt.show()
 
 
 
